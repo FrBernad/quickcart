a0bb472a4c2828b071207799edb1bf3facb634ef
igvazquez <igvazquez@itba.edu.ar>
2023-06-24 20:25:36 -0300

    Baisc Users API implemented
    Co-authored-by: Ignacio Agustin Manfredi <imanfredi@users.noreply.github.com>

--- users/users_service/src/api/users_api.py
+++ users/users_service/src/api/users_api.py
@@ -3,79 +3,70 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields, ValidationError
 
-app = Flask(__name__)
 
-users_bp = Blueprint("users", __name__, url_prefix="/users")
-    
-user = os.getenv('POSTGRES_USER')
-passwd = os.getenv('POSTGRES_PASSWORD')
-db = os.getenv('POSTGRES_DB')
-host = os.getenv('POSTGRES_SERVICE')
+def create_app():
+        
+    app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{user}:{passwd}@{host}:5432/{db}"
-db = SQLAlchemy(app)
+    users_bp = Blueprint("users", __name__, url_prefix="/users")
+        
+    user = os.getenv('POSTGRES_USER')
+    passwd = os.getenv('POSTGRES_PASSWORD')
+    db = os.getenv('POSTGRES_DB')
+    host = os.getenv('POSTGRES_SERVICE')
 
-@users_bp.route("/", methods=["POST"])
-def create_user():
-    try:
-        user = user_schema.load(request.json)
-    except ValidationError as e:
-        return jsonify(e.messages), 400
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user_schema.dump(user)), 201
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{user}:{passwd}@{host}:5432/{db}"
+    db = SQLAlchemy(app)
 
+    class User(db.Model):
+        __tablename__ = 'users'
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(50))
+        email = db.Column(db.String(50), unique=True)
 
-@users_bp.route("/<user_id>", methods=["GET"])
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify(user_schema.dump(user))
+        def __init__(self, name, email):
+            self.name = name
+            self.email = email
 
+    class UserSchema(Schema):
+        name = fields.Str(required=True)
+        email = fields.Email(required=True)
 
-@users_bp.route("/<user_id>", methods=["PUT"])
-def update_user(user_id):
-    return f"Update user with id ${user_id}", 200
+    user_schema = UserSchema()
+    users_schema = UserSchema(many=True)
 
+    @users_bp.route("", methods=["POST"])
+    def create_user():
+        data = request.get_json()
 
-app.register_blueprint(users_bp)
+        name = data.get('name')
+        email = data.get('email')
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50), unique=True)
+        if not name or not email:
+            return jsonify({'message': 'Missing required fields'}), 400
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+        # Create a new user object
+        new_user = User(name=name, email=email)
 
-class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    email = fields.Email(required=True)
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
-# @app.route('/users', methods=['GET'])
-# def get_users():
-#     users = User.query.all()
-#     return jsonify(users_schema.dump(users))
-
-# @app.route('/users/<int:user_id>', methods=['GET'])
-# def get_user(user_id):
-#     user = User.query.get_or_404(user_id)
-#     return jsonify(user_schema.dump(user))
-
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     try:
-#         user = user_schema.load(request.json)
-#     except ValidationError as e:
-#         return jsonify(e.messages), 400
-#     db.session.add(user)
-#     db.session.commit()
-#     return jsonify(user_schema.dump(user)), 201
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'id': new_user.id, 'name': new_user.name, 'email': new_user.email}), 201
 
 
-if __name__ == "__main__":
+    @users_bp.route("/<user_id>", methods=["GET"])
+    def get_user(user_id):
+        user = User.query.get_or_404(user_id)
+        return jsonify(user_schema.dump(user))
+
+
+    @users_bp.route("/<user_id>", methods=["PUT"])
+    def update_user(user_id):
+        return f"Update user with id ${user_id}", 200
+
+    app.register_blueprint(users_bp)
+
+    return app
+
+if __name__ == "__main__": 
+    app = create_app()
     app.run(debug=True, host=os.environ["EXPOSED_INTERFACES"])
