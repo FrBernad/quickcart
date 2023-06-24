d4fbf92f99be97630bce53320178feba5206e5f8
Francisco Bernad <frbernad@itba.edu.ar>
2023-06-24 18:10:24 +0000

    asdasd

--- users/users_service/src/.cicd/test.sh
+++ users/users_service/src/.cicd/test.sh
@@ -5,21 +5,15 @@ if [ "${TEST_TARGET:-}" = "INTEGRATION" ]; then
     # Execute your command here
     /usr/app/.venv/bin/gunicorn manage:app
 else
-
     ## pytest
     echo "Running tests for ${SERVICE_NAME} service"
-    python -m pytest "./tests" -v -s --junitxml=report.xml
+    python -m pytest "./tests" --junitxml=report.xml
 
     ## Coverage
     echo "Running coverage for ${SERVICE_NAME} service"
-    python -m pytest "./tests" -v -s -p no:warnings --cov="." --cov-report xml
+    python -m pytest "./tests" -p no:warnings --cov="." --cov-report xml
 
     ## Linting
     echo "Running linter for ${SERVICE_NAME} service"
     flake8 . --extend-ignore E221
-    # black src --check
-    # isort src --check
-
-    ## Security
-    # bandit -c .bandit.yml -r .
 fi
