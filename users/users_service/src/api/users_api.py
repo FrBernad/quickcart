3bd8666e6ee4cdb2b156322ae84c82a24ab2ff5d
igvazquez <igvazquez@itba.edu.ar>
2023-06-24 17:58:44 -0300

    DB for Users API
    Co-authored-by: Ignacio Agustin Manfredi <imanfredi@users.noreply.github.com>

--- users/users_service/src/api/users_api.py
+++ users/users_service/src/api/users_api.py
@@ -1,28 +1,60 @@
 from flask import Flask, jsonify, request, Blueprint
+import os
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields, ValidationError
 
-# from flask_sqlalchemy import SQLAlchemy
-# from marshmallow import Schema, fields, ValidationError
+app = Flask(__name__)
+
+users_bp = Blueprint("users", __name__, url_prefix="/users")
+    
+user = os.getenv('POSTGRES_USER')
+passwd = os.getenv('POSTGRES_PASSWORD')
+db = os.getenv('POSTGRES_DB')
+host = os.getenv('POSTGRES_SERVICE')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{user}:{passwd}@{host}:5432/{db}"
+db = SQLAlchemy(app)
+
+@users_bp.route("/", methods=["POST"])
+def create_user():
+    try:
+        user = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user_schema.dump(user)), 201
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-# db = SQLAlchemy(app)
+@users_bp.route("/<user_id>", methods=["GET"])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user_schema.dump(user))
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50))
-#     email = db.Column(db.String(50), unique=True)
 
-#     def __init__(self, name, email):
-#         self.name = name
-#         self.email = email
+@users_bp.route("/<user_id>", methods=["PUT"])
+def update_user(user_id):
+    return f"Update user with id ${user_id}", 200
 
-# class UserSchema(Schema):
-#     id = fields.Int(dump_only=True)
-#     name = fields.Str(required=True)
-#     email = fields.Email(required=True)
 
-# user_schema = UserSchema()
-# users_schema = UserSchema(many=True)
+app.register_blueprint(users_bp)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 # @app.route('/users', methods=['GET'])
 # def get_users():
@@ -43,27 +75,7 @@ from flask import Flask, jsonify, request, Blueprint
 #     db.session.add(user)
 #     db.session.commit()
 #     return jsonify(user_schema.dump(user)), 201
-app = Flask(__name__)
 
-users_bp = Blueprint("users", __name__, url_prefix="/users")
-
-
-@users_bp.route("/", methods=["POST"])
-def create_user():
-    return "Create user", 200
-
-
-@users_bp.route("/<user_id>", methods=["GET"])
-def get_user(user_id):
-    return f"Get user with id ${user_id}", 200
-
-
-@users_bp.route("/<user_id>", methods=["PUT"])
-def update_user(user_id):
-    return f"Update user with id ${user_id}", 200
-
-
-app.register_blueprint(users_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host=os.environ["EXPOSED_INTERFACES"])
