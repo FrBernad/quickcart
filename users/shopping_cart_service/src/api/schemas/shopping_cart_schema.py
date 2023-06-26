8dabd44360a4d4ba9b972c90071ab52380a39504
Ignacio Agustín Manfredi <imanfredi@itba.edu.ar>
2023-06-26 18:43:48 +0000

    asd

--- users/shopping_cart_service/src/api/schemas/shopping_cart_schema.py
+++ users/shopping_cart_service/src/api/schemas/shopping_cart_schema.py
@@ -1,9 +1,8 @@
 from marshmallow import Schema, fields
 
 class ShoppingCartSchema(Schema):
-    id = fields.Int(required=True)
-    name = fields.Str(required=True)
-    email = fields.Email(required=True)
+    user_id = fields.Int(required=True)
+    product_id = fields.Int(required=True)
 
 shopping_cart_schema = ShoppingCartSchema()
 shopping_carts_schema = ShoppingCartSchema(many=True)
