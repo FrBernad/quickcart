154c7625878bf7d5169c38636a73db549640419c
Francisco Bernad <frbernad@itba.edu.ar>
2023-06-29 16:51:20 +0000

    adsasd

--- users/users_service/src/.cicd/test.sh
+++ users/users_service/src/.cicd/test.sh
@@ -10,12 +10,12 @@ else
     echo "Running tests for ${SERVICE_NAME} service"
     python -m pytest "./src/tests" --junitxml=report.xml
 
-    # ## Coverage
-    # echo "Running coverage for ${SERVICE_NAME} service"
-    # python -m pytest "./src/tests" -p no:warnings --cov="." --cov-report xml
+    ## Coverage
+    echo "Running coverage for ${SERVICE_NAME} service"
+    python -m pytest "./src/tests" -p no:warnings --cov="." --cov-report xml
 
-    # ## Linting
-    # echo "Running linter for ${SERVICE_NAME} service"
-    # flake8 . --extend-ignore E221
+    ## Linting
+    echo "Running linter for ${SERVICE_NAME} service"
+    flake8 . --extend-ignore E221
 
 fi
