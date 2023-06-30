d28933091b226064ffea15c7d53bdc15a91bf04f
Joaquin Legammare <jlegammare@gmail.com>
2023-06-30 19:50:50 -0300

    feat: shopping cart controller unit test

--- users/shopping_cart_service/src/tests/unit/controllers/shopping_controller_test.py
+++ users/shopping_cart_service/src/tests/unit/controllers/shopping_controller_test.py
@@ -1,32 +1,66 @@
 import pytest
-import json
+from flask import url_for
+from src.api.interfaces.services.shopping_cart_service import ShoppingCartService
+from src.api.models.shopping_cart import ShoppingCarts
 
-def test_shopping_cart_controller():
-    print("test passed")
+## --------       GET SHOPPING CART    --------
 
-# def test_create_user(test_client, monkeypatch):
-#     pass
+def test_get_shopping_cart_products(monkeypatch):
 
+    def mock_get_user_by_id(user_id):
+        user = User(username="test", email="test@test.com", password="12345678")
+        user.id = 1
+        return user if user.id = '1' else None
 
-# def test_get_existing_user_by_id(test_client, monkeypatch):
-#     pass
-    # def mock_get_user_by_id(user_id):
-    #     return {
-    #         "id": 1,
-    #         "name": "test",
-    #         "email": "test@test.com",
-    #     }
+    def mock_get_shopping_cart_by_user_id(user_id):
+        return ShoppingCarts(user_id=1, product_id=1, quantity=1)
 
-    # monkeypatch.setattr(UserService, "get_user_by_id", mock_get_user_by_id)
-    # resp = test_client.get("/users/1")
+    monkeypatch.setattr(ShoppingCartService, 'get_user_by_id', mock_get_user_by_id)
+    monkeypatch.setattr(ShoppingCartService, 'get_shopping_cart_by_user_id', mock_get_shopping_cart_by_user_id)
 
-    # data = json.loads(resp.data.decode())
-    # assert resp.status_code == 200
-    # assert "1" in data["id"]
-    # assert "test" in data["username"]
-    # assert "test@test.com" in data["email"]
-    # assert "password" not in data
+    test_app = app.test_client()
 
+    # Test with existing user
+    response = test_app.get(url_for('shopping_cart.get_shopping_cart_products', user_id='1'))
+    assert response.status_code == 200
 
-# def test_update_user(test_client, monkeypatch):
-#     pass
+    # Test with non-existing user
+    response = test_app.get(url_for('shopping_cart.get_shopping_cart_products', user_id='2'))
+    assert response.status_code == 404
+
+## --------       GET SHOPPING CART PRODUCTS     --------
+
+def test_get_shopping_cart_products(monkeypatch):
+
+    def mock_get_user_by_id(user_id):
+        user = User(username="test", email="test@test.com", password="12345678")
+        user.id = 1
+        return user if user.id = '1' else None
+
+    def mock_get_shopping_cart_by_user_id(user_id):
+        return ShoppingCarts(user_id=1, product_id=1, quantity=1)
+
+    monkeypatch.setattr(ShoppingCartService, 'get_user_by_id', mock_get_user_by_id)
+    monkeypatch.setattr(ShoppingCartService, 'get_shopping_cart_by_user_id', mock_get_shopping_cart_by_user_id)
+
+    test_app = app.test_client()
+
+    response = test_app.get(url_for('shopping_cart.get_shopping_cart_products', user_id='1'))
+    assert response.status_code == 200
+
+    response = test_app.get(url_for('shopping_cart.get_shopping_cart_products', user_id='2'))
+    assert response.status_code == 404
+
+## --------       DELETE SHOPPING CART    --------
+
+def test_delete_shopping_cart(monkeypatch):
+
+    def mock_delete_shopping_cart(user_id):
+        return None
+
+    monkeypatch.setattr(ShoppingCartService, 'delete_shopping_cart', mock_delete_shopping_cart)
+
+    test_app = app.test_client()
+
+    response = test_app.delete(url_for('shopping_cart.delete_shopping_cart', user_id='1'))
+    assert response.status_code == 204
