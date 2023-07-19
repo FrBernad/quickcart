7bd16d98796bf6626c3a7a8d2f5e79aaa9c49057
Francisco Bernad <frbernad@itba.edu.ar>
2023-07-19 17:53:58 +0000

    fix: change exception name

--- users/shopping_cart_service/src/api/interfaces/exceptions/user_not_found_exception.py
+++ users/shopping_cart_service/src/api/interfaces/exceptions/user_not_found_exception.py
@@ -1,6 +1,6 @@
 from src.api.interfaces.exceptions.generic_api_exception import GenericApiException
 
 
-class SpecificApiException(GenericApiException):
+class UserNotFoundException(GenericApiException):
     def __init__(self, user_id):
         super().__init__(f"""User with id {user_id} not found.""", 400)
