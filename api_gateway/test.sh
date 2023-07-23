90382c870f5f608c1543878ef72328cfe488e325
Francisco Bernad <frbernad@itba.edu.ar>
2023-07-23 18:16:17 +0000

    asd

--- api_gateway/test.sh
+++ api_gateway/test.sh
@@ -9,12 +9,12 @@ if [[ "${ENV:-}" =~ ^qa ]]; then
     echo "Running interface tests for ${SERVICE_NAME}"
     export BUILD_ID=$(date +%Y%m%d%H%M%S)
     python -m pytest "./tests/interface" -p no:warnings --junitxml=interface_${SERVICE_NAME}_report.xml
-    # interface_tests_exit_status=$?
-
-    # if [ $integration_tests_exit_status -eq 0 ] && [ $interface_tests_exit_status -eq 0 ]; then
-    #     exit 0
-    # else
-    #     exit 1
-    # fi
+    interface_tests_exit_status=$?
+    exit 1
+    if [ $integration_tests_exit_status -eq 0 ] && [ $interface_tests_exit_status -eq 0 ]; then
+        exit 0
+    else
+        exit 1
+    fi
 
 fi
