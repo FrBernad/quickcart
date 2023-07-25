1769afa55481925c05ea22b88bad7070d78b3f9e
igvazquez <igvazquez@itba.edu.ar>
2023-07-25 18:55:37 -0300

    product dao test

--- products/products_service/src/tests/unit/persistence/products_dao_test.py
+++ products/products_service/src/tests/unit/persistence/products_dao_test.py
@@ -1,54 +1,64 @@
 import pytest
 from src.api.models.product import Product
+from src.api.models.tag import Tag
 from unittest.mock import MagicMock
 
 
+def test_create_product(monkeypatch, test_product_dao):
+    product_db_model_mock = MagicMock()
+    all_mock = MagicMock()
+    all_mock.all = lambda: []
+    
+    product_dao, db_mock = test_product_dao
 
-# def test_create_product(monkeypatch, test_product_dao):
-#     mock_products = [ShoppingCartProduct(user_id=1, product_id=1, quantity=1)]
+    monkeypatch.setattr(
+        "src.api.persistence.product_dao_impl.Product",
+        product_db_model_mock,
+    )
 
-#     shopping_cart_db_model_mock = MagicMock()
-#     query_mock = MagicMock()
-#     all_mock = MagicMock()
-#     all_mock.all = lambda: mock_products
-#     query_mock.filter_by = lambda user_id: all_mock
-#     shopping_cart_db_model_mock.query = query_mock
-#     monkeypatch.setattr(
-#         "src.api.persistence.shopping_cart_dao_impl.ShoppingCartProduct",
-#         shopping_cart_db_model_mock,
-#     )
+    def mock_scalars(*args, **kwargs):
+        return all_mock
 
-#     shopping_cart_dao, db_mock = test_shopping_cart_dao
+    # Patch the actual database query with the mock implementation
+    monkeypatch.setattr(product_dao.db.session, 'scalars', mock_scalars)
 
-#     user_id = 1
+    user_id = 1
+    name = "iPhone"
+    price = 1200.5
+    category_id = 1
+    tags = ['black']
+    stock = 5
 
-#     products = shopping_cart_dao.get_products(user_id)
+    product_dao.create_product(user_id, name, price, category_id, tags, stock)
 
-#     assert len(mock_products) == len(products)
-#     assert mock_products[0].user_id == products[0].user_id
-#     assert mock_products[0].product_id == products[0].product_id
-#     assert mock_products[0].quantity == products[0].quantity
+    db_mock.session.add.assert_called_once()
+    db_mock.session.commit.assert_called_once()
 
+def test_create_product_existing_tag(monkeypatch, test_product_dao):
+    product_db_model_mock = MagicMock()
+    all_mock = MagicMock()
+    all_mock.all = lambda: [Tag(id=1, name='black')]
+    
+    product_dao, db_mock = test_product_dao
 
-# def test_delete_product(monkeypatch, test_product_dao):
+    monkeypatch.setattr(
+        "src.api.persistence.product_dao_impl.Product",
+        product_db_model_mock,
+    )
 
-   
+    def mock_scalars(*args, **kwargs):
+        return all_mock
 
-# def get_products(monkeypatch, test_product_dao):
+    monkeypatch.setattr(product_dao.db.session, 'scalars', mock_scalars)
 
+    user_id = 1
+    name = "iPhone"
+    price = 1200.5
+    category_id = 1
+    tags = ['black']
+    stock = 5
 
+    product_dao.create_product(user_id, name, price, category_id, tags, stock)
 
-
-
-# def get_product_by_id(monkeypatch, test_product_dao):
-
-
-# def update_product(monkeypatch, test_product_dao):
-
-
-# def update_product_score(monkeypatch, test_product_dao):
-
-# def update_product_stock(monkeypatch, test_product_dao):
-
-# def _get_and_generate_tags(monkeypatch, test_product_dao):
-       
+    db_mock.session.add.assert_called_once()
+    db_mock.session.commit.assert_called_once()
